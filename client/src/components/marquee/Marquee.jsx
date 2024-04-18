7e5c186b07c219330ac98394359a60d1d9330202
Bruno Grecchi <b_grecchi@hotmail.com>
2024-04-18 22:13:18 +0100

    stack logos added

---
+++ client/src/components/marquee/Marquee.jsx
@@ -0,0 +1,8 @@
+import "./marquee.css";
+export default function Marquee() {
+  return (
+    <div className="marqueeWrapper">
+      <h2 className="marqueeh2">TECH STACK - TECH STACK - TECH STACK</h2>;
+    </div>
+  );
+}
--- client/src/views/stack/Stack.jsx
+++ client/src/views/stack/Stack.jsx
@@ -1,9 +1,41 @@
 import "./stack.css";
+// import js from "../../../public/logos/01javascript.svg";
+
+import Marquee from "../../components/marquee/Marquee";
+
+const logos = [
+  "01Javascript.svg",
+  "02React.svg",
+  "03HTML.svg",
+  "04CSS.svg",
+  "05Node.svg",
+  "06Mongo.svg",
+  "07Express.svg",
+  "08Framer.svg",
+  "09Bootstrap.svg",
+  "10SaSS.svg",
+  "11GitHub.svg",
+];
 
 export default function Stack() {
   return (
     <section className="tech-stack">
-      <h1>TECH STACK</h1>
+      <Marquee />
+      {/* <h3>TECHNOLOGIES I WORK WITH </h3> */}
+
+      <section className="logos">
+        {logos.map((fileName, index) => (
+          <div className="logoWrapper" key={index}>
+            <img
+              key={index}
+              src={"../../../public/logos/" + fileName}
+              alt={`Image ${index}`}
+              className="gallery-image"
+            />
+            <p>{fileName.substring(2, fileName.length - 4)}</p>
+          </div>
+        ))}
+      </section>
     </section>
   );
 }
