bd327a26bec7465a23b461c12f6db95de551cca6
Bruno Grecchi <bgrecchi@Brunos-MacBook-Pro.local>
2024-07-04 23:10:46 +0100

    updated project elements and fixed open/close function

--- client/src/views/works/Works.jsx
+++ client/src/views/works/Works.jsx
@@ -7,9 +7,11 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 
 let initialState = {
-  drumMachine: false,
   pinkPiranha: false,
-  taraShipping: false,
+  whatsNew: false,
+  drumMachine: false,
+  portfolio: false,
+  fromValidation: false,
 };
 
 const gearIconAnimation = {
@@ -24,10 +26,6 @@ const fadeInAnimation = {
   whileInView: { opacity: 1, scale: 1 },
   transition: { duration: 0.7, ease: "easeInOut", type: "spring" },
   viewport: { once: true },
-
-  // open: { opacity: 1, height: "auto", scale: 1 },
-  // closed: { opacity: 0, height: 0, scale: 0.9 },
-  // transition: { duration: 0.5, ease: "easeInOut" },
 };
 
 export default function Works() {
@@ -53,6 +51,7 @@ export default function Works() {
         <h2>WORKS</h2>
       </motion.div>
 
+      {/* --------- GEAR ICON ANIMATION-------------- */}
       <div className="gearIconWrapper">
         <motion.img
           src={gearIcon}
@@ -74,8 +73,9 @@ export default function Works() {
         />
       </div>
 
-      {/* WORKS UL */}
+      {/* ----------- WORKS UL WRAPPER -------------------*/}
       <ul className="worksUL">
+        {/* --------- PINK PIRANHA -------------- */}
         <motion.li
           {...fadeInAnimation}
           transition={{ ...fadeInAnimation.transition, delay: 1 }}
@@ -89,7 +89,7 @@ export default function Works() {
                 transform: isClicked.pinkPiranha
                   ? "rotate(180deg)"
                   : "rotate(0deg)",
-                transition: "transform .5",
+                transition: "transform .3s",
               }}
               alt="Arrow Up"
               onClick={() => handleClick("pinkPiranha")}
@@ -147,7 +147,291 @@ export default function Works() {
           )}
         </motion.li>
 
-        {/*------------- * project -------------- */}
+        {/* --------- WHATS NEW-------------- */}
+        <motion.li
+          {...fadeInAnimation}
+          transition={{ ...fadeInAnimation.transition, delay: 1 }}
+        >
+          <div className="projectTitle">
+            <p> WHATS NEW</p>
+            <img
+              src={ArrowUpSolid}
+              className="arrowUp"
+              style={{
+                transform: isClicked.whatsNew
+                  ? "rotate(180deg)"
+                  : "rotate(0deg)",
+                transition: "transform .3s",
+              }}
+              alt="Arrow Up"
+              onClick={() => handleClick("whatsNew")}
+            />
+          </div>
+
+          {isClicked.whatsNew && (
+            <motion.div className="project" {...fadeInAnimation}>
+              <div className="projectDescription">
+                <div className="projectText">
+                  <h4>PROJECT HIGHLIGHTS</h4>
+
+                  <ul className="projectStack">
+                    <li> REACT </li>
+                    <li> TAILWIND </li>
+                    <li> NEXT JS</li>
+                  </ul>
+
+                  <p>
+                    For this project I led the front-end development,
+                    translating a designer`s layout into a fully functional
+                    website.
+                  </p>
+                  <p>
+                    I opted to use Next.js due to its server-side rendering
+                    capabilities to boost SEO. For the UI components, I chose
+                    Tailwind CSS for efficient and scalable styling.
+                  </p>
+                  <p>
+                    One of the main challenges was adapting the design, which
+                    was provided only in a desktop format. To address this, I
+                    created responsive versions to ensure a seamless experience
+                    across all devices.
+                  </p>
+                  <p>
+                    Finally, I used Framer Motion for a bit of animation and
+                    movement to enhance the user experience.
+                  </p>
+                </div>
+
+                <div className="buttons">
+                  <button>
+                    SOURCE CODE <img src="../../../public/logos/11github.svg" />
+                  </button>
+                  <button>
+                    VIEW LIVE
+                    <img src={arrowSide} className="openArrow" alt="" />
+                  </button>
+                </div>
+              </div>
+
+              {/*add project img here  */}
+              <img src={projectMockup} className="projectImage"></img>
+            </motion.div>
+          )}
+        </motion.li>
+
+        {/* --------- DRUM MACHINE------------- */}
+        <motion.li
+          {...fadeInAnimation}
+          transition={{ ...fadeInAnimation.transition, delay: 1 }}
+        >
+          <div className="projectTitle">
+            <p> DRUM MACHINE</p>
+            <img
+              src={ArrowUpSolid}
+              className="arrowUp"
+              style={{
+                transform: isClicked.drumMachine
+                  ? "rotate(180deg)"
+                  : "rotate(0deg)",
+                transition: "transform .3s",
+              }}
+              alt="Arrow Up"
+              onClick={() => handleClick("drumMachine")}
+            />
+          </div>
+
+          {isClicked.drumMachine && (
+            <motion.div className="project" {...fadeInAnimation}>
+              <div className="projectDescription">
+                <div className="projectText">
+                  <h4>PROJECT HIGHLIGHTS</h4>
+
+                  <ul className="projectStack">
+                    <li> REACT </li>
+                    <li> TAILWIND </li>
+                    <li> NEXT JS</li>
+                  </ul>
+
+                  <p>
+                    For this project I led the front-end development,
+                    translating a designer`s layout into a fully functional
+                    website.
+                  </p>
+                  <p>
+                    I opted to use Next.js due to its server-side rendering
+                    capabilities to boost SEO. For the UI components, I chose
+                    Tailwind CSS for efficient and scalable styling.
+                  </p>
+                  <p>
+                    One of the main challenges was adapting the design, which
+                    was provided only in a desktop format. To address this, I
+                    created responsive versions to ensure a seamless experience
+                    across all devices.
+                  </p>
+                  <p>
+                    Finally, I used Framer Motion for a bit of animation and
+                    movement to enhance the user experience.
+                  </p>
+                </div>
+
+                <div className="buttons">
+                  <button>
+                    SOURCE CODE <img src="../../../public/logos/11github.svg" />
+                  </button>
+                  <button>
+                    VIEW LIVE
+                    <img src={arrowSide} className="openArrow" alt="" />
+                  </button>
+                </div>
+              </div>
+
+              {/*add project img here  */}
+              <img src={projectMockup} className="projectImage"></img>
+            </motion.div>
+          )}
+        </motion.li>
+        {/* --------- MY PORTFOLIO------------- */}
+        <motion.li
+          {...fadeInAnimation}
+          transition={{ ...fadeInAnimation.transition, delay: 1 }}
+        >
+          <div className="projectTitle">
+            <p> MY PORTFOLIO</p>
+            <img
+              src={ArrowUpSolid}
+              className="arrowUp"
+              style={{
+                transform: isClicked.portfolio
+                  ? "rotate(180deg)"
+                  : "rotate(0deg)",
+                transition: "transform .3s",
+              }}
+              alt="Arrow Up"
+              onClick={() => handleClick("portfolio")}
+            />
+          </div>
+
+          {isClicked.portfolio && (
+            <motion.div className="project" {...fadeInAnimation}>
+              <div className="projectDescription">
+                <div className="projectText">
+                  <h4>PROJECT HIGHLIGHTS</h4>
+
+                  <ul className="projectStack">
+                    <li> REACT </li>
+                    <li> TAILWIND </li>
+                    <li> NEXT JS</li>
+                  </ul>
+
+                  <p>
+                    For this project I led the front-end development,
+                    translating a designer`s layout into a fully functional
+                    website.
+                  </p>
+                  <p>
+                    I opted to use Next.js due to its server-side rendering
+                    capabilities to boost SEO. For the UI components, I chose
+                    Tailwind CSS for efficient and scalable styling.
+                  </p>
+                  <p>
+                    One of the main challenges was adapting the design, which
+                    was provided only in a desktop format. To address this, I
+                    created responsive versions to ensure a seamless experience
+                    across all devices.
+                  </p>
+                  <p>
+                    Finally, I used Framer Motion for a bit of animation and
+                    movement to enhance the user experience.
+                  </p>
+                </div>
+
+                <div className="buttons">
+                  <button>
+                    SOURCE CODE <img src="../../../public/logos/11github.svg" />
+                  </button>
+                  <button>
+                    VIEW LIVE
+                    <img src={arrowSide} className="openArrow" alt="" />
+                  </button>
+                </div>
+              </div>
+
+              {/*add project img here  */}
+              <img src={projectMockup} className="projectImage"></img>
+            </motion.div>
+          )}
+        </motion.li>
+        {/* --------- FORM VALIDATION------------- */}
+        <motion.li
+          {...fadeInAnimation}
+          transition={{ ...fadeInAnimation.transition, delay: 1 }}
+        >
+          <div className="projectTitle">
+            <p> FORM VALIDATION</p>
+            <img
+              src={ArrowUpSolid}
+              className="arrowUp"
+              style={{
+                transform: isClicked.fromValidation
+                  ? "rotate(180deg)"
+                  : "rotate(0deg)",
+                transition: "transform .3s",
+              }}
+              alt="Arrow Up"
+              onClick={() => handleClick("fromValidation")}
+            />
+          </div>
+
+          {isClicked.fromValidation && (
+            <motion.div className="project" {...fadeInAnimation}>
+              <div className="projectDescription">
+                <div className="projectText">
+                  <h4>PROJECT HIGHLIGHTS</h4>
+
+                  <ul className="projectStack">
+                    <li> REACT </li>
+                    <li> TAILWIND </li>
+                    <li> NEXT JS</li>
+                  </ul>
+
+                  <p>
+                    For this project I led the front-end development,
+                    translating a designer`s layout into a fully functional
+                    website.
+                  </p>
+                  <p>
+                    I opted to use Next.js due to its server-side rendering
+                    capabilities to boost SEO. For the UI components, I chose
+                    Tailwind CSS for efficient and scalable styling.
+                  </p>
+                  <p>
+                    One of the main challenges was adapting the design, which
+                    was provided only in a desktop format. To address this, I
+                    created responsive versions to ensure a seamless experience
+                    across all devices.
+                  </p>
+                  <p>
+                    Finally, I used Framer Motion for a bit of animation and
+                    movement to enhance the user experience.
+                  </p>
+                </div>
+
+                <div className="buttons">
+                  <button>
+                    SOURCE CODE <img src="../../../public/logos/11github.svg" />
+                  </button>
+                  <button>
+                    VIEW LIVE
+                    <img src={arrowSide} className="openArrow" alt="" />
+                  </button>
+                </div>
+              </div>
+
+              {/*add project img here  */}
+              <img src={projectMockup} className="projectImage"></img>
+            </motion.div>
+          )}
+        </motion.li>
       </ul>
     </section>
   );
